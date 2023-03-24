4873d839aeddb8a30dbb63fa1b7e0d7cffd714cd
DrStein <rene.okt@gmail.com>
2023-03-24 18:23:28 -0400

    Main

---
+++ ejerciciosTema4_1.js
@@ -0,0 +1,14 @@
+/*
+Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+Pista: Los números inferiores a 0 son negativos y los superiores, positivos
+*/
+
+var numeroIf = -10;
+var response;
+
+if (numeroIf >= 0) {
+    response = "El valor de la variable numeroIf es positivo"
+} else {
+    response = "El valor de la variable numeroIf es negativo";
+}
+console.log(response);
